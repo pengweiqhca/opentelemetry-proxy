92bf82b0d11826b7f4c216131170a5a2f588d2db
pengweiqhca <pengweiqhca@sina.com>
2023-04-11 14:01:38 +0800

    Write log

--- src/OpenTelemetry.StaticProxy.Fody/ModuleWeaver.cs
+++ src/OpenTelemetry.StaticProxy.Fody/ModuleWeaver.cs
@@ -6,12 +6,24 @@ namespace OpenTelemetry.StaticProxy.Fody;
 // https://github.com/vescon/MethodBoundaryAspect.Fody
 public class ModuleWeaver : BaseModuleWeaver
 {
-    public override void Execute() => new StaticProxyEmitter(new(
-        ModuleDefinition,
-        ResolveMainModule("System.Diagnostics.DiagnosticSource"),
-        ResolveMainModule("OpenTelemetry"),
-        ResolveMainModule("OpenTelemetry.Api"),
-        ResolveMainModule("OpenTelemetry.Proxy"))).Emit();
+    public override void Execute()
+    {
+        string? version = null;
+
+        var location = typeof(ModuleWeaver).Assembly.Location;
+        if (File.Exists(location))
+            version = FileVersionInfo.GetVersionInfo(location).ProductVersion?.Split('-')[0];
+
+        if (string.IsNullOrWhiteSpace(version)) version = typeof(ModuleWeaver).Assembly.GetName().Version?.ToString();
+
+        if (!string.IsNullOrWhiteSpace(version)) WriteInfo($"OpenTelemetry.StaticProxy version: {version}");
+
+        new StaticProxyEmitter(new(ModuleDefinition,
+            ResolveMainModule("System.Diagnostics.DiagnosticSource"),
+            ResolveMainModule("OpenTelemetry"),
+            ResolveMainModule("OpenTelemetry.Api"),
+            ResolveMainModule("OpenTelemetry.Proxy"))).Emit();
+    }
 
     private ModuleDefinition ResolveMainModule(string assemblyName)
     {
