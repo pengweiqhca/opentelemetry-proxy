e397e82bdb2070fcf36d4a546f16e97f8e9e5d22
pengweiqhca <pengweiqhca@qq.com>
2024-01-18 16:08:40 +0800

    Add

---
+++ src/OpenTelemetry.Proxy/TracerProviderBuilderExtensions.cs
@@ -0,0 +1,9 @@
+﻿using OpenTelemetry.Trace;
+
+namespace OpenTelemetry.Proxy;
+
+public static class TracerProviderBuilderExtensions
+{
+    public static TracerProviderBuilder AddActivityNameProcessor(this TracerProviderBuilder builder) =>
+        builder.AddProcessor(new ActivityNameProcessor());
+}
